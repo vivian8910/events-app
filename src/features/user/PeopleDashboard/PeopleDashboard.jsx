938e122375ab1930c7bbee605d3d60a525d6ec7a
yangx <james891013@outlook.com>
2018-08-31 11:52:18 +0200

    Configure routes

---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PeopleDashboard = () => {
+  return (
+    <div>
+      <h1>People Dash Board</h1>
+    </div>
+  )
+}
+
+export default PeopleDashboard
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
@@ -9,7 +10,12 @@ import registerServiceWorker from './registerServiceWorker';
 const appEl = document.getElementById('app');
 
 let render = () => {
-    ReactDOM.render(<App />, appEl)
+    ReactDOM.render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>,
+        appEl
+    )
 }
 
 if (module.hot) {
